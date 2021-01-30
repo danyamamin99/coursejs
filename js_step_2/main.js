930519aa4d72e8cedcaf40ff7bf3e0a3427fa717
Danya <example@gmail.com>
2021-01-31 00:04:04 +0300

    js_step_2

---
+++ js_step_2/main.js
@@ -0,0 +1,60 @@
+"use strict";
+/* Задание на урок:
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
+str.length - и получить её длину)
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let film, rating, i = 1;
+
+do {
+    film = prompt("Один из последних просмотренных фильмов?", "");
+    rating = prompt("На сколько оцените его?", "");
+
+    if (film != "" && rating != "" && film.length < 50 && film != null && rating != null) {
+        personalMovieDB.movies[film] = rating;
+        i++;
+    } else {
+        alert("Введите название фильма! И не отменяйте вопрос!");
+    }
+} while (i <= 2);
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+console.log(personalMovieDB);
+
+if (personalMovieDB.count <= 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
