1dea95483ae951d49096e162dfb1e05b5244102d
Danya <example@gmail.com>
2021-02-02 23:07:01 +0300

    js_step_4_new

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const obj = {
-  a: 11,
-  b: 22
-};
-
-const newObj = Object.create(obj);
-// Object.setPrototypeOf(newObj, obj);
-console.log(newObj.a);
-console.log(newObj.b);
-newObj.a = 123;
-console.log(newObj);
-console.log(obj);
+console.log(null || 2 && 3 || 4 );
